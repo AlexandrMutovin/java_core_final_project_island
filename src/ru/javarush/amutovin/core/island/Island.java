a718f086c5511c391f5cbf0ce75a0ebdb7cee6e5
Alexandr Mutovin <alexandr.mutovin@gmail.com>
2022-08-28 00:26:33 +0300

    Show Statistic

--- src/ru/javarush/amutovin/core/island/Island.java
+++ src/ru/javarush/amutovin/core/island/Island.java
@@ -6,6 +6,9 @@ import ru.javarush.amutovin.core.island.Animals.Animal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class Island {
     @Getter
@@ -34,14 +37,17 @@ public class Island {
         stringBuilder.append("Остров содержит: ");
         stringBuilder.append(separator);
         stringBuilder.append("Размер острова (количество клеток x, y): ");
-        stringBuilder.append(sizeX + " " + sizeY);
+        stringBuilder.append(sizeX + " X " + sizeY);
         stringBuilder.append(separator);
         stringBuilder.append("Количество животных на острове: ");
         stringBuilder.append(getAnimalCount());
         stringBuilder.append(separator);
+        stringBuilder.append("Из них: ");
+        stringBuilder.append(separator);
+        stringBuilder.append(getUniqueAnimalCount());
         stringBuilder.append("Количество растений на острове: ");
         stringBuilder.append(getPlantCount());
-        return "";
+        return stringBuilder.toString();
     }
 
     private int getAnimalCount() {
@@ -64,6 +70,31 @@ public class Island {
 
         }
         return plantCount;
+    }
+
+    private String getUniqueAnimalCount(){
+        Map<String, Integer>  mapAminal = new TreeMap<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        List<Animal> allAnimalList = new ArrayList<>();
+
+        for (int x = 0; x < cells.length; x++) {
+            for (int y = 0; y < cells[x].length; y++) {
+                allAnimalList.addAll(cells[x][y].getAnimalList());
+            }
+
+            mapAminal = allAnimalList.stream()
+            .collect(Collectors.toMap(key -> key.getClass().getSimpleName(),
+             value -> 1, (value, value2) -> (int)value + 1,
+             TreeMap::new
+             ));
+
+        }
+
+        for (Map.Entry<String, Integer> pair: mapAminal.entrySet()) {
+            stringBuilder.append(pair.getKey() + " = " + pair.getValue());
+            stringBuilder.append(System.getProperty("line.separator"));
+        }
+        return stringBuilder.toString();
     }
 
 }
--- src/ru/javarush/amutovin/core/island/Main.java
+++ src/ru/javarush/amutovin/core/island/Main.java
@@ -1,13 +1,9 @@
 package ru.javarush.amutovin.core.island;
 
-import ru.javarush.amutovin.core.island.Animals.Animal;
-import ru.javarush.amutovin.core.island.Animals.Carnivores.Bear;
-import ru.javarush.amutovin.core.island.Animals.Herbivores.Goat;
-
 public class Main {
     public static void main(String[] args) {
-        Island island = InitialIsland.getInstance(1,1);
-
+        Island island = InitialIsland.getInstance(10,20);
+        System.out.println(island.getStatistic());
     }
 
 
